1d83019d43880bdc2fb4a2a04c41e75d4cbef69f
Ng Yew Wei <122435876+Yew-Wei@users.noreply.github.com>
2024-04-10 21:24:42 +0800

    Add files via upload
    Pulled the latest changes (reviews page) and added a Edit Profile page to the route.

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Authentication from '../views/AuthenticationPage.vue'
 import About from '../views/About.vue'
 import Project from '../views/Project.vue'
 import ProjProfile from '../components/ProjProfile.vue'
-import SignUpQuestionaire from '../views/SignUpQuestionaire.vue'
+import SignUpQuestionaire from '../components/SignUpQuestionaire.vue'
 import Profile from '../components/Profile.vue'
-import Reviews from "../views/ReviewsPage.vue"
+import EditProfile from '../components/EditProfile.vue';
+
 
 const routes = [
     {
@@ -19,12 +20,12 @@ const routes = [
         component: Project
     },
     {
-        path: '/project/:id/:varibale',
+        path: '/project/:id',
         name: 'ProjProfile',
         component: ProjProfile
     },
     {
-        path: '/auth/:state',
+        path: '/auth',
         name: 'Auth',
         component: Authentication,
     },
@@ -40,10 +41,18 @@ const routes = [
         component: Profile,
         props:true 
     },
+
     {
         path:'/reviews',
         name: 'Reviews',
         component: Reviews
+    },
+
+    {
+        path: '/Edit-Profile/:userId',
+        name: 'EditProfile',
+        component: EditProfile,
+        props: true
     }
 ]
 
