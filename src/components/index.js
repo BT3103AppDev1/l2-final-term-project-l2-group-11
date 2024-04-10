98d77f23095b5c4b3ec14d28711533e8501f8937
Ng Yew Wei <122435876+Yew-Wei@users.noreply.github.com>
2024-04-10 21:14:16 +0800

    Add files via upload
    Forgot to commit the updated version of index.js previously, it now contains the path to edit profile page.

---
+++ src/components/index.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Authentication from '../views/AuthenticationPage.vue'
+import About from '../views/About.vue'
+import Project from '../views/Project.vue'
+import ProjProfile from '../components/ProjProfile.vue'
+import SignUpQuestionaire from '../components/SignUpQuestionaire.vue'
+import Profile from '../components/Profile.vue'
+import EditProfile from '../components/EditProfile.vue';
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/project',
+        name: 'Project',
+        component: Project
+    },
+    {
+        path: '/project/:id',
+        name: 'ProjProfile',
+        component: ProjProfile
+    },
+    {
+        path: '/auth',
+        name: 'Auth',
+        component: Authentication,
+    },
+    {
+        path: '/SignUpQuestionaire/:userId',
+        name: 'SignUpQuestionaire',
+        component: SignUpQuestionaire,
+        props: true
+    },
+    {
+        path:'/Profile/:userId',
+        name: 'Profile',
+        component: Profile,
+        props:true 
+    },
+
+    {
+        path: '/Edit-Profile/:userId',
+        name: 'EditProfile',
+        component: EditProfile,
+        props: true
+    }
+]
+
+const router = createRouter({
+ history: createWebHistory(),
+ routes
+})
+
+export default router
