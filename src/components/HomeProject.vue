10bda95e4e76ac32e627ff515cb7381da394aa20
Phuong181003 <sokaimtp181003@gmail.com>
2024-04-19 03:26:16 +0800

    update HomeProject.vue

--- src/components/HomeProject.vue
+++ src/components/HomeProject.vue
@@ -1,12 +1,17 @@
 <template>
-    <section class="projects">
-      <div class="intro"><span id="title">Our Projects</span></div>
-      <div class="projects-container">
-        <div class="projects-cart">
-          <Card v-for="project in projects" :key="project.id" :project="project" :image-url="project.projectImage"/>
-        </div>
+  <section class="projects">
+    <div class="intro"><span id="title">Our Projects</span></div>
+    <div class="projects-container">
+      
+      <div class="projects-cart">
+        <Card v-for="(project, index) in projects.slice(0, 4)" :key="project.id" :project="project"
+          :image-url="project.projectImage" />
+        <h3 class = "view" @click = "viewMore">View more...</h3>
+      </div>
+      
     </div>
-    </section>
+
+  </section>
 </template>
 
 <script>
@@ -46,41 +51,54 @@ export default {
         console.error("Error fetching projects: ", error);
       }
     },
+    viewMore() {
+      this.$router.push("/project");
+    }
   }
 }
 </script>
 
 <style scoped>
-  .intro {
-    text-align: center;
-  }
+.intro {
+  text-align: center;
+}
 
-  .projects{
-    background-color:white;
-    color:black;
-  }
-  
-  #title {
-    text-align: center;
-  
-    font-size: 45px;
-    font-family: Inter;
-    font-weight: 600;
-   
-    word-wrap: break-word;
-  }
-  .projects-cart {
+.projects {
+  background-color: white;
+  color: black;
+}
+
+#title {
+  text-align: center;
+
+  font-size: 45px;
+  font-family: Inter;
+  font-weight: 600;
+
+  word-wrap: break-word;
+}
+
+.projects-cart {
   height: 350px;
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
   gap: 8px;
   overflow: scroll;
-  }
-  .projects-container {
+}
+
+.projects-container {
   margin-left: 50px;
   font-size: 25px;
   font-weight: 550px;
-  }
+}
 
+.view {
+  text-align: center;
+  font-size: 23px;
+  font-weight: 550px;
+  cursor: pointer;
+  padding-left: 15px;
+  padding-top: 100px;
+}
 </style>
