45c7b300b0bf21b3d278938ff0f61a52f0ce730e
Tan Xuan Zheng <xztan123@gmail.com>
2024-04-21 17:02:41 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,45 @@ import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-    <Navbar />
+    <Navbar :profileImageUrl="profileImageUrl" />
     <div class = router-view>
-        <router-view/>
+        <router-view @profile-updated="updateProfileImageUrl"/>
     </div>
 </template>
   
-  <script>  
-    import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";
-    
+<script>  
+import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";
+import firebaseApp from './Firebase.js';
+import firebase from './uifire.js';
+import 'firebase/compat/auth';
+import { getFirestore } from "firebase/firestore";
+import { collection, getDoc, doc, deleteDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+
+const db = getFirestore(firebaseApp);
   export default {
     name: 'App',
+    data() {
+      return {
+        uid:'',
+        profileImageUrl: '',
+      };
+    },
+    methods: {
+      updateProfileImageUrl(newImageUrl) {
+        this.profileImageUrl = newImageUrl;
+      },
+    },
+
+    mounted() {
+      firebase.auth().onAuthStateChanged(async (user) => {
+        if (user) {
+          this.uid = user.uid;
+          const docRef = doc(db, "User Information", user.uid);
+          const docSnap = await getDoc(docRef);
+          this.profileImageUrl = docSnap.data().profileImageUrl
+        }
+      })
+    }          
   }
   </script>
   
